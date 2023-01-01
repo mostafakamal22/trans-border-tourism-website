42ce37aeec78f38f0493cbd14a22a64db752c519
mostafakamal22 <36553185+mostafakamal22@users.noreply.github.com>
2023-01-01 22:03:26 +0200

    finish header section styles and functionalities

---
+++ scripts/index.js
@@ -0,0 +1,33 @@
+//Handling Open/Close Mobile Navigation
+const closeBtn = document.getElementById("close");
+const openBtn = document.getElementById("open");
+const mobileNav = document.getElementById("mobile-navigation");
+const navbar = document.getElementById("navbar");
+const serviceSection = document.getElementById("services");
+
+closeBtn.addEventListener("click", () => {
+  mobileNav.classList.toggle("translate-x-full");
+});
+
+openBtn.addEventListener("click", () => {
+  mobileNav.classList.toggle("translate-x-full");
+});
+
+//Handle Navbar Background Color Change While Scrolling.
+window.addEventListener("scroll", () => {
+  const yOffset = window.scrollY;
+
+  //If navbar at the top of the page.
+  if (!yOffset) {
+    navbar.classList.remove("bg-black", "shadow-lg");
+  }
+
+  //If we scroll add black background
+  if (
+    yOffset &&
+    !navbar.classList.contains("bg-black", "shadow-lg") &&
+    yOffset >= window.outerHeight
+  ) {
+    navbar.classList.add("bg-black", "shadow-lg");
+  }
+});
